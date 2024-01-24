8a421515b2d96ca9d5172fbf64b232dead529765
gerardcoma <gerard.coma@soidemdt.com>
2024-01-24 11:09:53 +0100

    redirects

--- next.config.js
+++ next.config.js
@@ -3,6 +3,25 @@ const nextConfig = {
    experimental: {
       esmExternals: "loose",
       serverComponentsExternalPackages: ["mongoose", "bcryptjs"]
+   },
+   async redirects() {
+      return [
+         {
+            source: '/',
+            destination: `/dashboard/${process.env.DASHBOARD_DEFAULT_LINE}/${process.env.DASHBOARD_DEFAULT_NAME}`,
+            permanent: false,
+         },
+         {
+            source: '/dashboard',
+            destination: `/dashboard/${process.env.DASHBOARD_DEFAULT_LINE}/${process.env.DASHBOARD_DEFAULT_NAME}`,
+            permanent: false,
+         },
+         {
+            source: '/dashboard/:line',
+            destination: `/dashboard/:line/${process.env.DASHBOARD_DEFAULT_NAME}`,
+            permanent: false,
+         }
+      ];
    }
 }
 
