eb06ee7eb435a484a97b09063116a80288c1956a
Gerard Coma <gerard.coma@soidemdt.com>
2024-01-24 20:22:12 +0100

    SpeedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import GetNav from './routing'
 import { NextAuthProvider } from "./providers";
 import { getSession } from "@/services/session"
@@ -24,6 +25,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
                {children}
             </NextAuthProvider>
             <Analytics />
+            <SpeedInsights />
          </body>
       </html>
    )
