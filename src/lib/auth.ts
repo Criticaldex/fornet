1f972b90c0ab5abd719f46b4f25d2b8665f77901
Gerard Coma <gerard.coma@soidemdt.com>
2024-02-09 10:53:14 +0100

    deelete signin page

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -44,6 +44,9 @@ export const authOptions: NextAuthOptions = {
          }
       }),
    ],
+   pages: {
+      signIn: "/auth/signin"
+   },
    callbacks: {
       session: ({ session, token }) => {
          // console.log("Session Callback", { session, token });
