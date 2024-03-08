df868fdda3c6f61a227e605c07744517b004b262
gerardcoma <criticaldex@gmail.com>
2024-03-08 12:13:31 +0100

    mock intl working

--- next.config.js
+++ next.config.js
@@ -4,25 +4,25 @@ const nextConfig = {
       esmExternals: "loose",
       serverComponentsExternalPackages: ["mongoose", "bcryptjs"]
    },
-   async redirects() {
-      return [
-         {
-            source: '/',
-            destination: `/dashboard/${process.env.DASHBOARD_DEFAULT_LINE}/${process.env.DASHBOARD_DEFAULT_NAME}`,
-            permanent: false,
-         },
-         {
-            source: '/dashboard',
-            destination: `/dashboard/${process.env.DASHBOARD_DEFAULT_LINE}/${process.env.DASHBOARD_DEFAULT_NAME}`,
-            permanent: false,
-         },
-         {
-            source: '/dashboard/:line',
-            destination: `/dashboard/:line/${process.env.DASHBOARD_DEFAULT_NAME}`,
-            permanent: false,
-         }
-      ];
-   }
+   // async redirects() {
+   //    return [
+   //       {
+   //          source: '/',
+   //          destination: `/dashboard/${process.env.DASHBOARD_DEFAULT_LINE}/${process.env.DASHBOARD_DEFAULT_NAME}`,
+   //          permanent: false,
+   //       },
+   //       {
+   //          source: '/dashboard',
+   //          destination: `/dashboard/${process.env.DASHBOARD_DEFAULT_LINE}/${process.env.DASHBOARD_DEFAULT_NAME}`,
+   //          permanent: false,
+   //       },
+   //       {
+   //          source: '/dashboard/:line',
+   //          destination: `/dashboard/:line/${process.env.DASHBOARD_DEFAULT_NAME}`,
+   //          permanent: false,
+   //       }
+   //    ];
+   // }
 }
 
 module.exports = nextConfig
