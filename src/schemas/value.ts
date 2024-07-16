c293d5ab06725cfeed5ad63260cbb4f86c7d09bb
gerardcoma <criticaldex@gmail.com>
2024-07-16 13:11:35 +0200

    small fixes label i values

--- src/schemas/value.ts
+++ src/schemas/value.ts
@@ -1,19 +1,19 @@
 import mongoose from 'mongoose'
 
 export interface ValueIface {
-    name: string,
-    line: string
+   line: string,
+   name?: string
 }
 
 const ValueSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'Name is mandatory!']
-    },
-    line: {
-        type: String,
-        required: [true, 'Line is mandatory!']
-    }
+   name: {
+      type: String,
+      required: [false, 'Name is mandatory!']
+   },
+   line: {
+      type: String,
+      required: [true, 'Line is mandatory!']
+   }
 });
 
 export default ValueSchema;
