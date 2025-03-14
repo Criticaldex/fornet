ebacc39f1b00e0ccfef9d090ef7262b98f36593a
Criticaldex <criticaldex@gmail.com>
2025-03-14 13:24:09 +0100

    update line as well

--- src/app/api/plcs/[db]/sensors/route.ts
+++ src/app/api/plcs/[db]/sensors/route.ts
@@ -22,7 +22,6 @@ export async function PATCH(request: Request, { params }: { params: { db: string
       }
 
       const filter = {
-         line: body.line,
          plc_name: body.plc_name
       }
 
