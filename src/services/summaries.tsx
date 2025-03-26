8309542d44a66c87c4df719385b17263ff27b967
Criticaldex <criticaldex@gmail.com>
2025-03-26 10:50:08 +0100

    selector any

---
+++ src/services/summaries.tsx
@@ -0,0 +1,90 @@
+import _ from "lodash"
+import { getSession } from "@/services/session"
+import { SummaryIface } from "@/schemas/summary";
+
+const getSummaries = async (filter: any, fields?: string[], db?: string) => {
+   if (!db) {
+      const session = await getSession();
+      db = session?.user.db;
+   }
+
+   if (!fields) {
+      fields = ['-_id'];
+   }
+
+   return fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/summaries/${db}`,
+      {
+         method: 'POST',
+         headers: {
+            'Content-type': 'application/json',
+            token: `${process.env.NEXT_PUBLIC_API_KEY}`,
+         },
+         body: JSON.stringify(
+            {
+               fields: fields,
+               filter: filter,
+               sort: 'timestamp'
+            }
+         ),
+      }).then(res => res.json());
+}
+
+export const getChartSummaries = async (filter: SummaryIface) => {
+   const data: [] = await getSummaries(filter);
+   const summaries = [{
+      name: 'Productividad',
+      data: [] as any
+   }];
+
+   data.forEach((i: any) => {
+      let val = [i.timestamp, i.value];
+      summaries[0].data.push(val);
+   })
+   return summaries;
+}
+
+export const getLines = async (db?: any) => {
+   const fields = ['-_id', 'line']
+   const data = await getSummaries({}, fields, db);
+   let groupBySec = _.groupBy(data, 'line');
+   let lines: string[] = [];
+   for (const [key, value] of (Object.entries(groupBySec) as [string, any][])) {
+      lines.push(key);
+   }
+   return lines;
+}
+
+export const getNames = async (filter?: SummaryIface, db?: any) => {
+   const fields = ['-_id', 'name', 'unit']
+   const data = await getSummaries(filter, fields, db);
+   let groupByName = _.groupBy(data, 'name');
+
+   let names: string[] = [];
+   let units: string[] = [];
+   for (const [key, value] of (Object.entries(groupByName) as [string, any][])) {
+      names.push(key);
+      units.push(value[0].unit);
+   }
+   return { names, units };
+}
+
+export const deleteSummaries = async (filter: SummaryIface, db: string | undefined) => {
+   return fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/summaries/${db}`,
+      {
+         method: 'DELETE',
+         headers: {
+            'Content-type': 'application/json',
+            token: `${process.env.NEXT_PUBLIC_API_KEY}`,
+         },
+         body: JSON.stringify(filter)
+      }).then(res => res.json());
+}
+export const getYears = async () => {
+   const data = await getSummaries({}, ['year']);
+   const yearsGroup = _.groupBy(data, 'year');
+   let years: string[] = []
+   for (const [key, value] of (Object.entries(yearsGroup) as [string, any][])) {
+      years.push(key);
+   }
+   return years;
+}
