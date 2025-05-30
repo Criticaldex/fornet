1c634681dc8e85ebf2b559ce82a9d5eb799d1716
Gerard Coma <criticaldex@gmail.com>
2025-05-30 17:24:16 +0200

    get tokens auto

--- src/app/powerbi/page.tsx
+++ src/app/powerbi/page.tsx
@@ -1,24 +1,21 @@
-'use client'
 import { PowerBi } from "./powerbi";
 import { PbiForm } from "./form";
 import { useForm } from "react-hook-form";
 import { useState } from "react";
-export default function Dashboard() {
+import { getEmbedUrl, getToken } from "@/services/powerbi";
+import { delay } from "lodash";
+export default async function Dashboard() {
 
-   const [entraToken, setEntraToken] = useState('');
-   const [embedURL, setEmbedURL] = useState('');
+   const { token } = await getToken("fbca2019-9f7d-4355-bc45-d0a909660154", "5615fe53-6829-41c9-913c-60f82bf0bb8e");
+   const { embedUrl } = await getEmbedUrl('d8792594-3b20-4154-9892-77fe3e41f43e', '5615fe53-6829-41c9-913c-60f82bf0bb8e');
 
    return (
       <>
          <div className="flex flex-grow-max mx-2 mb-2">
             <div className="flex flex-col grow p-2 bg-bgLight rounded-md ">
-               <PbiForm
-                  setEntraToken={setEntraToken}
-                  setEmbedURL={setEmbedURL}
-               />
                <PowerBi
-                  entraToken={entraToken}
-                  embedURL={embedURL}
+                  entraToken={token}
+                  embedURL={embedUrl}
                />
             </div>
          </div>
