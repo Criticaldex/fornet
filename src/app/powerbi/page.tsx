32cc6fedb9c467dc3179cc21c2ea3af5e523e760
Valls, Lluis <Lluis.Valls@kps.com>
2025-06-15 21:34:42 +0200

    S'ha de modificar la variable de env que hi ha a powerbi.tsx y page.tsx del power

--- src/app/powerbi/page.tsx
+++ src/app/powerbi/page.tsx
@@ -2,20 +2,23 @@ import { PowerBi } from "./powerbi";
 import { PbiForm } from "./form";
 import { useForm } from "react-hook-form";
 import { useState } from "react";
-import { getEmbedUrl, getToken } from "@/services/powerbi";
+import { getEmbedUrl, getToken, getEntraToken } from "@/services/powerbi";
 import { delay } from "lodash";
 export default async function Dashboard() {
 
-   const response = await getToken("fbca2019-9f7d-4355-bc45-d0a909660154", "5615fe53-6829-41c9-913c-60f82bf0bb8e");
-   const response2 = await getEmbedUrl('d8792594-3b20-4154-9892-77fe3e41f43e', '5615fe53-6829-41c9-913c-60f82bf0bb8e');
+   const response1 = await getEntraToken(process.env.AZURE_TENANTID);
+   const response = await getToken('26c2b827-3906-46c7-98a5-b1affecfe86a', '756c3a2c-6b3e-4877-b488-2409fa990d15', response1);
+   const response2 = await getEmbedUrl('f0f6519f-717e-4881-a515-7b76fbd0e7e4', '756c3a2c-6b3e-4877-b488-2409fa990d15', response1);
 
    return (
       <>
          <div className="h-full flex flex-col mx-2 mb-2">
             <div className="flex mt-auto flex-col grow justify-end h-screen p-2 bg-bgLight rounded-md">
                <PowerBi
-                  entraToken={'token'}
-                  embedURL={'embedUrl'}
+                  // entraToken={'token'}
+                  // embedURL={'embedUrl'}
+                  entraToken={response.token}
+                  embedURL={response2.embedUrl}
                />
             </div>
          </div>
--- src/services/powerbi.tsx
+++ src/services/powerbi.tsx
@@ -1,16 +1,44 @@
 import _ from "lodash"
 import { NodeIface } from "@/schemas/node";
 import { getSession } from "./session";
+// const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IkNOdjBPSTNSd3FsSEZFVm5hb01Bc2hDSDJYRSIsImtpZCI6IkNOdjBPSTNSd3FsSEZFVm5hb01Bc2hDSDJYRSJ9.eyJhdWQiOiJodHRwczovL2FuYWx5c2lzLndpbmRvd3MubmV0L3Bvd2VyYmkvYXBpIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvMWMzYzdhN2MtM2ZlNS00NmM0LTg0YTktMTY3OWRiZjQzNGE0LyIsImlhdCI6MTc1MDAxMjE3OSwibmJmIjoxNzUwMDEyMTc5LCJleHAiOjE3NTAwMTYwNzksImFpbyI6ImsyUmdZRERjVW4zRmMxc2VrNTJHamtIVkJ6MEdBQT09IiwiYXBwaWQiOiJiMDU3OWI1YS1kM2FlLTQwODItYjNhOC0xZDNmZGY3NTRhNWYiLCJhcHBpZGFjciI6IjEiLCJpZHAiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC8xYzNjN2E3Yy0zZmU1LTQ2YzQtODRhOS0xNjc5ZGJmNDM0YTQvIiwiaWR0eXAiOiJhcHAiLCJvaWQiOiI0ZThmYjcwZC03ODcxLTQ0NzEtOWFiZC04MWIzYjlkZjdhMGQiLCJyaCI6IjEuQVU4QWZIbzhIT1VfeEVhRXFSWjUyX1EwcEFrQUFBQUFBQUFBd0FBQUFBQUFBQUE1QVFCUEFBLiIsInJvbGVzIjpbIlRlbmFudC5SZWFkV3JpdGUuQWxsIiwiVGVuYW50LlJlYWQuQWxsIl0sInN1YiI6IjRlOGZiNzBkLTc4NzEtNDQ3MS05YWJkLTgxYjNiOWRmN2EwZCIsInRpZCI6IjFjM2M3YTdjLTNmZTUtNDZjNC04NGE5LTE2NzlkYmY0MzRhNCIsInV0aSI6InE0LXVEbFBMT0VxRHZQcUowM2dVQUEiLCJ2ZXIiOiIxLjAiLCJ4bXNfZnRkIjoiVVlaUTd5NllJLVVyN3JodGRDMzQzVnAwb283LXoxZHNrWlB5TXNnZURNMEJabkpoYm1ObFl5MWtjMjF6IiwieG1zX2lkcmVsIjoiNyAxNiIsInhtc19yZCI6IjAuNDJMbFlCSmk5QmNTNFdBWEV0Z2xrX0hXOFdLaDl6NHRucnM2MGVIUGdLS2NRZ0pXLTJkcEJaMzc1N1ozLXB1Ylo5MlAxUUpGT1lRRU9Ca2c0QUNVQmdBIn0.KBcA8rRJDpIapPbSDr9lMbqH1azV7a8T2IhwE27Zk1H-nnaTQ0JKwM_zZXF880v7Ks7BkdaQ2UqclqvJf2EQkSYSxGq2F_RBeS-s41mxtt8TGovGtKsZ3nIzSG7wM8dRs541H8oq07mUKpu1ZvCYKwy6h5fYjKOK8WlZNcuAjjxirCkjQxLMOMrYGXNUNSK7qqR0ViovnsVEoVTrYxSA8Xc-9KejVbFe2LhHAdnAYQtU4_Qp16OyzZedbJhBEni4-YSRrkGedrft3lhw6raVSwTba2o0MyAwTg4iChQgCjccjzW7YowFiSfsu5v0S5mPxp3UWDIPNEO1Q7M0M5p9Ug'
 
-const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IkNOdjBPSTNSd3FsSEZFVm5hb01Bc2hDSDJYRSIsImtpZCI6IkNOdjBPSTNSd3FsSEZFVm5hb01Bc2hDSDJYRSJ9.eyJhdWQiOiJodHRwczovL2FuYWx5c2lzLndpbmRvd3MubmV0L3Bvd2VyYmkvYXBpIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvMTAzMTQ4OGEtYWUxZC00MmY5LWEwYmYtMDVlNzc1MTE0NmVhLyIsImlhdCI6MTc0ODYxODg2NywibmJmIjoxNzQ4NjE4ODY3LCJleHAiOjE3NDg2MjI3NjcsImFpbyI6ImsyUmdZTERJTXk1My9uem55NlV6VE90RkJDVFlBUT09IiwiYXBwaWQiOiI4YTlkMjQ1Mi0xNTM0LTQwNzEtYWJlYS1lODliMTQ2YjNmYTYiLCJhcHBpZGFjciI6IjEiLCJpZHAiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC8xMDMxNDg4YS1hZTFkLTQyZjktYTBiZi0wNWU3NzUxMTQ2ZWEvIiwiaWR0eXAiOiJhcHAiLCJvaWQiOiI4NmM4NjJhMS1jOGM2LTRkYjgtYWIwYy02ZWQ3ZGFkNzU1Y2YiLCJyaCI6IjEuQWE0QWlrZ3hFQjJ1LVVLZ3Z3WG5kUkZHNmdrQUFBQUFBQUFBd0FBQUFBQUFBQUNyQUFDdUFBLiIsInN1YiI6Ijg2Yzg2MmExLWM4YzYtNGRiOC1hYjBjLTZlZDdkYWQ3NTVjZiIsInRpZCI6IjEwMzE0ODhhLWFlMWQtNDJmOS1hMGJmLTA1ZTc3NTExNDZlYSIsInV0aSI6ImdzTmhtVy1XaWtLalVFczdJUE5rQUEiLCJ2ZXIiOiIxLjAiLCJ4bXNfZnRkIjoiRUhDdGlJWWhTMFhUSFVLeWRxS1NEakVHaS1PdVhLY2M1QTR2Q2M3YnZRb0JabkpoYm1ObFl5MWtjMjF6IiwieG1zX2lkcmVsIjoiNyAyOCIsInhtc19yZCI6IjAuNDJMallCSmlXc2NvSk1MQkxpUXd4V1Z2dVhISlI0LXBCejZXaW9ZNkhnS0tjZ29KNUhuRjVmOE9EWEpkcXJ0WnhFcF9ZVEpRbEVOSWdKTUJBZzVBYVFBIn0.HYSy5bn959fan8SZpmHt6l0KauDxt34nRiUrAS_v-_46Aq6y7cG3fR8N0pecOfhgEq21gEj3rlYEbrJ1QaCPx3bYWIl8m00_0zYDQewbza1GDnJgcuxXTnyCuOy7hKit5-tmq6-oVN9OnCaN7ksWfT9MS-ePrs5jcdinlk4HmkphW_hRy064LbDkel2tvaLXQTESkWod7hqvWeZMQW2Dmx-inQU9jOwMumfka9FnzPeYzmQ61LwF4I7JurH6MH9KkTM6-CsofSzo6w8q_mdIKP5XxhSB2ZCs8Y6-R7is3bNj-kbIoS0LSUU854m9N1ji7sCr97mmchCkL7uGFHwX4Q'
+export const getEntraToken = async (tenantId: string) => {
+   const clientId = process.env.AZURE_CLIENTID;
+   const clientSecret = process.env.AZURE_CLIENTSECRET;
+   const scope = "https://analysis.windows.net/powerbi/api/.default";
 
-export const getToken = async (dataset: string, report: string) => {
+   const data = new URLSearchParams({
+      grant_type: "client_credentials",
+      client_id: clientId,
+      client_secret: clientSecret,
+      scope: scope,
+   });
+
+   const response = await fetch(`https://login.microsoftonline.com/${tenantId}/oauth2/v2.0/token`, {
+      method: "POST",
+      headers: {
+         "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: data,
+   });
+
+   if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(`Error al obtener el token: ${JSON.stringify(errorData, null, 2)}`);
+   }
+
+   const result = await response.json();
+   return result.access_token;
+};
+
+export const getToken = async (dataset: string, report: string, Token: string) => {
    return fetch('https://api.powerbi.com/v1.0/myorg/GenerateToken',
       {
          method: 'POST',
          headers: {
             'Content-type': 'application/json',
-            'Authorization': `Bearer ${token}`
+            'Authorization': `Bearer ${Token}`
          },
          body: JSON.stringify(
             {
@@ -30,13 +58,13 @@ export const getToken = async (dataset: string, report: string) => {
       }).then(res => res.json());
 }
 
-export const getEmbedUrl = async (groupId: string, reportId: string) => {
+export const getEmbedUrl = async (groupId: string, reportId: string, Token: string) => {
    return fetch(`https://api.powerbi.com/v1.0/myorg/groups/${groupId}/reports/${reportId}`,
       {
          method: 'GET',
          headers: {
             'Content-type': 'application/json',
-            'Authorization': `${token}`
+            'Authorization': `Bearer ${Token}`
          }
       }).then(res => res.json());
 }
