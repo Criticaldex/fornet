aa9ea5e52b6b27b30e7565c338bc49ec1ee26fb2
Valls, Lluis <Lluis.Valls@kps.com>
2025-06-16 05:51:35 +0200

    Listo to merge

--- src/app/powerbi/page.tsx
+++ src/app/powerbi/page.tsx
@@ -6,10 +6,9 @@ import { getEmbedUrl, getToken, getEntraToken } from "@/services/powerbi";
 import { delay } from "lodash";
 export default async function Dashboard() {
 
-   const response1 = await getEntraToken(process.env.AZURE_TENANTID);
-   const response = await getToken('26c2b827-3906-46c7-98a5-b1affecfe86a', '756c3a2c-6b3e-4877-b488-2409fa990d15', response1);
-   const response2 = await getEmbedUrl('f0f6519f-717e-4881-a515-7b76fbd0e7e4', '756c3a2c-6b3e-4877-b488-2409fa990d15', response1);
-
+   const AzureEntraToken = await getEntraToken(process.env.AZURE_TENANTID!);
+   const Token = await getToken('26c2b827-3906-46c7-98a5-b1affecfe86a', '756c3a2c-6b3e-4877-b488-2409fa990d15', AzureEntraToken);
+   const EmbededUrl = await getEmbedUrl('f0f6519f-717e-4881-a515-7b76fbd0e7e4', '756c3a2c-6b3e-4877-b488-2409fa990d15', AzureEntraToken);
    return (
       <>
          <div className="h-full flex flex-col mx-2 mb-2">
@@ -17,8 +16,8 @@ export default async function Dashboard() {
                <PowerBi
                   // entraToken={'token'}
                   // embedURL={'embedUrl'}
-                  entraToken={response.token}
-                  embedURL={response2.embedUrl}
+                  entraToken={Token}
+                  embedURL={EmbededUrl}
                />
             </div>
          </div>
