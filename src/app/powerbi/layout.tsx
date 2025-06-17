c9ba494bea91a6af9a40fe78a26e06d56c5908a8
Gerard Coma <criticaldex@gmail.com>
2025-06-17 18:58:26 +0200

    cleanup

--- src/app/powerbi/layout.tsx
+++ src/app/powerbi/layout.tsx
@@ -1,12 +1,4 @@
-import { getSession } from "@/services/session";
-import { GetLinksYears } from "./routing";
-import { FaCog } from "react-icons/fa"
-import Link from "next/link";
-import { getYears } from "@/services/summaries";
-
 export default async function MqttLayout({ children }: any) {
-   const session = await getSession();
-   const years = await getYears();
    return (
       <div>
          <title>Fornet | PowerBi</title>
