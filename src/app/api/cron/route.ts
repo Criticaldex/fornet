8c00efe8795ba891d213cc2ff94eebd89243651f
Gerard Coma <criticaldex@gmail.com>
2025-07-15 13:30:37 +0200

    fix script

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -7,43 +7,22 @@ export async function GET() {
     //    console.log('ERROR: Bad Auth');
     //    return NextResponse.json({ ERROR: 'Bad Auth' }, { status: 401 });
     //}
-    console.log('Beginning Cron!!');
 
-    const interval = 24;
-    let timestamp = Math.floor(Date.now() - (interval * 60 * 60 * 1000));
+    try {
+        console.log('Beginning Cron!!');
 
-    const fields = [
-        "-_id"
-    ];
+        const interval = 24;
+        let timestamp = Math.floor(Date.now() - (interval * 60 * 60 * 1000));
 
-    let summaries: SummaryIface[] = [];
-    console.log('fetching sensors...');
-    empreses.forEach(async dbName => {
-        const sensors = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/sensors/${dbName}`,
-            {
-                method: 'POST',
-                headers: {
-                    'Content-type': 'application/json',
-                    token: `${process.env.NEXT_PUBLIC_API_KEY}`,
-                },
-                body: JSON.stringify(
-                    {
-                        fields: fields,
-                        filter: { read: true }
-                    }
-                ),
-            }).then(res => res.json());
-        console.log('sensord done!', sensors);
+        const fields = [
+            "-_id"
+        ];
 
-        for (const sensor of sensors) {
-            const filter = {
-                "timestamp": { $gte: timestamp },
-                "line": sensor.line,
-                "name": sensor.name
-            };
-            console.log('fetching values!!');
+        let summaries: SummaryIface[] = [];
+        console.log('fetching sensors...');
 
-            const values = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/values/${dbName}`,
+        for (const dbName of empreses) {
+            const sensors = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/sensors/${dbName}`,
                 {
                     method: 'POST',
                     headers: {
@@ -53,63 +32,91 @@ export async function GET() {
                     body: JSON.stringify(
                         {
                             fields: fields,
-                            filter: filter,
-                            sort: 'timestamp'
+                            filter: { read: true }
                         }
                     ),
                 }).then(res => res.json());
-            console.log('values done!!', values);
+            console.log('sensord done!', sensors);
 
-            if (values[0]) {
-                let summary: SummaryIface = {
-                    line: values[0].line,
-                    plc_name: values[0].plc_name,
-                    name: values[0].name,
-                    unit: values[0].unit,
-                    max: values[0].value,
-                    min: values[0].value,
-                    avg: 0,
-                    year: new Date(timestamp).getFullYear(),
-                    month: new Date(timestamp).getMonth(),
-                    day: new Date(timestamp).getDate(),
-                }
-                for (const value of values) {
-                    if (value.value != undefined && summary.max != undefined && summary.min != undefined && summary.avg != undefined) {
-                        summary.max = (value.value > summary.max) ? value.value : summary.max;
-                        summary.min = (value.value < summary.min) ? value.value : summary.min;
-                        summary.avg += value.value;
-                    }
+            for (const sensor of sensors) {
+                const filter = {
+                    "timestamp": { $gte: timestamp },
+                    "line": sensor.line,
+                    "name": sensor.name
                 };
-                summary.avg = (summary.avg) ? parseFloat((summary.avg / values.length).toFixed(2)) : 0;
-                summaries.push(summary);
-            }
-        };
-        console.log('inserting summaries...');
+                console.log('fetching values!!');
 
-        const insert = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/summaries/${dbName}`,
-            {
-                method: 'PATCH',
-                headers: {
-                    'Content-type': 'application/json',
-                    token: `${process.env.NEXT_PUBLIC_API_KEY}`,
-                },
-                body: JSON.stringify(summaries)
-            }).then(res => res.json());
-        if (insert.ERROR) {
-            return NextResponse.json(insert, { status: 500 })
-        } else {
-            const deleteValues = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/values/${dbName}`,
+                const values = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/values/${dbName}`,
+                    {
+                        method: 'POST',
+                        headers: {
+                            'Content-type': 'application/json',
+                            token: `${process.env.NEXT_PUBLIC_API_KEY}`,
+                        },
+                        body: JSON.stringify(
+                            {
+                                fields: fields,
+                                filter: filter,
+                                sort: 'timestamp'
+                            }
+                        ),
+                    }).then(res => res.json());
+                console.log('values done!!', values);
+
+                if (values[0]) {
+                    let summary: SummaryIface = {
+                        line: values[0].line,
+                        plc_name: values[0].plc_name,
+                        name: values[0].name,
+                        unit: values[0].unit,
+                        max: values[0].value,
+                        min: values[0].value,
+                        avg: 0,
+                        year: new Date(timestamp).getFullYear(),
+                        month: new Date(timestamp).getMonth(),
+                        day: new Date(timestamp).getDate(),
+                    }
+                    for (const value of values) {
+                        if (value.value != undefined && summary.max != undefined && summary.min != undefined && summary.avg != undefined) {
+                            summary.max = (value.value > summary.max) ? value.value : summary.max;
+                            summary.min = (value.value < summary.min) ? value.value : summary.min;
+                            summary.avg += value.value;
+                        }
+                    };
+                    summary.avg = (summary.avg) ? parseFloat((summary.avg / values.length).toFixed(2)) : 0;
+                    summaries.push(summary);
+                }
+            };
+            console.log('inserting summaries...');
+
+            const insert = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/summaries/${dbName}`,
                 {
-                    method: 'DELETE',
+                    method: 'PATCH',
                     headers: {
                         'Content-type': 'application/json',
                         token: `${process.env.NEXT_PUBLIC_API_KEY}`,
                     },
-                    body: JSON.stringify({})
+                    body: JSON.stringify(summaries)
                 }).then(res => res.json());
-            console.log('Values deleted: ', deleteValues);
+            if (insert.ERROR) {
+                return NextResponse.json(insert, { status: 500 })
+            } else {
+                const deleteValues = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/values/${dbName}`,
+                    {
+                        method: 'DELETE',
+                        headers: {
+                            'Content-type': 'application/json',
+                            token: `${process.env.NEXT_PUBLIC_API_KEY}`,
+                        },
+                        body: JSON.stringify({})
+                    }).then(res => res.json());
+                console.log('Values deleted: ', deleteValues);
+            }
+            console.log('insert done!!', insert);
         }
-        console.log('insert done!!', insert);
-    });
-    return NextResponse.json('OK');
+        return NextResponse.json('OK');
+    } catch (error) {
+        console.error('Cron job failed:', error);
+        return NextResponse.json({ ERROR: 'Internal server error' }, { status: 500 });
+    }
 }
